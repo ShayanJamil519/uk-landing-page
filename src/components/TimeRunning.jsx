b2d6820c394c2d069695f5c0d440e90b235002c4
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-19 00:57:21 +0500

    UI Fixes

--- src/components/TimeRunning.jsx
+++ src/components/TimeRunning.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function TimeRunning() {
   return (
     <div className="flex items-center justify-center">
-      <div className="text-white mt-12 mb-3 uppercase font-helvetica px-4">
-        <h2 className="text-[15px] font-[700] text-center">
+      <div className="text-white mt-5 mb-3 uppercase font-helvetica px-1">
+        <h2 className="text-[13px] font-bold text-center">
           Time Is Running Out. Reserve Your Seat Now!
         </h2>
-        <h3 className="text-[12px] text-[#BDBDBD] text-center">
+        <h3 className="text-[10px] text-[#BDBDBD] text-center">
           Fee will increase to{" "}
           <span
             style={{
