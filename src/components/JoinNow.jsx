9ad226176a6b4405abe306549a29e69a86afafd1
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-19 09:35:01 +0500

    Join Now Section  Responsiveness Done

--- src/components/JoinNow.jsx
+++ src/components/JoinNow.jsx
@@ -17,12 +17,12 @@ const JoinNow = () => {
       )}
 
       <div className=" flex justify-center items-center">
-        <div className="w-[90%] min-h-[224px] lg:w-[30%] mx-auto   justify-center items-center flex  rounded-[32px] relative">
+        <div className="w-[90%]  min-h-[224px] lg:h-auto lg:w-[50%] mx-auto   justify-center items-center flex  rounded-[32px] relative">
           <img
             onClick={handleOpen}
             src="/image 5.svg"
             alt="logo"
-            className="absolute right-0 top-0 bottom-0  left-0 w-full  cursor-pointer  "
+            className="absolute right-0 top-0 bottom-0  left-0 sm:relative w-full   cursor-pointer  "
           />
           {/* <div className="cursor-pointer z-10 bg-[#000] rounded-md py-2 px-4">
           <IoMdPlay className="text-[#fff] text-[20px]" />
