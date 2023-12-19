c3a9be36116e370ee8ac82e4b8511c0f5457ed34
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-19 10:14:09 +0500

    Join Workshop Section  Responsiveness Done

--- src/components/JoinWorkshop.jsx
+++ src/components/JoinWorkshop.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const JoinWorkshop = () => {
   return (
-    <div className="w-full mt-7 mb-5 font-helvetica text-center">
+    <div className="w-full mt-7 lg:mt-16 md:mt-12 sm:mt-10 mb-5 font-helvetica text-center">
       {/* Top */}
       <div
-        className="w-[90%] mx-auto rounded-[13px] text-[11px] text-[#fff] font-bold py-2 px-1"
+        className="w-[90%] lg:w-[55%] md:w-[65] sm:w-[75%] mx-auto rounded-[13px] sm:rounded-[20px] text-[11px] lg:text-[20px] md:text-[17px] sm:text-[15px] text-[#fff] font-bold py-2 px-1"
         style={{
           background:
             "linear-gradient(247deg, #F77 1.21%, #D62323 1.22%, #D75931 102%)",
         }}
       >
         <p className="m-0">JOIN THE ONE PERSON AGENCY WORKSHOP NOW!</p>
-        <p className="m-0">(ONLY ₹299)</p>
+        <p className="m-0 italic">(ONLY ₹299)</p>
       </div>
       {/* Bottom */}
-      <div className="mt-10 relative px-4 grid grid-cols-2 gap-5 ">
+      <div className="mt-10 lg:mt-24 md:mt-16 sm:mt-12 relative px-4 md:px-20 sm:px-12 lg:px-28 grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 ">
         <img
           src="/worksop__layer.svg"
           alt="logo"
-          className="absolute right-0 -top-5"
+          className="absolute right-0 -top-5 md:hidden block"
         />
 
         <div
-          className="py-4 px-3 relative text-[#D9D6D6] text-[11px] font-helvetica col-span-1"
+          className="py-4 sm:py-7 lg:py-10 px-3 relative text-[#D9D6D6] text-[11px] lg:text-[20px] md:text-[17px] sm:text-[14px] font-helvetica col-span-1"
           style={{
             borderRadius: "19px",
             boxShadow: "-2px -2px 3px 0px #FF7A00",
@@ -32,21 +32,21 @@ const JoinWorkshop = () => {
             backdropFilter: "blur(2.7000px)",
           }}
         >
-          <span className="w-[8px] h-[8px] rounded-md bg-[#fff] absolute top-4 left-4"></span>
+          <span className="w-[8px] h-[8px] sm:w-[10px] sm:h-[10px] rounded-md bg-[#fff] absolute top-4 left-4  sm:top-6 sm:left-6"></span>
 
           <img
             src="/workshop__icon__1.svg"
             alt="logo"
-            className="mx-auto mb-1"
+            className="mx-auto mb-1 lg:mb-6 sm:mb-4 lg:w-[80px] md:w-[60px] sm-w-[40px]"
           />
-          <p>Launch Your</p>
+          <p className="-mb-1">Launch Your</p>
           <p>
             High Ticket <span className="font-bold ml-1 italic">Agency</span>
           </p>
           <img src="/Line 1.svg" alt="logo" className="mx-auto mt-2" />
         </div>
         <div
-          className="py-4 px-3 relative text-[#D9D6D6] text-[11px] font-helvetica col-span-1"
+          className="py-4 sm:py-7 lg:py-10 px-3 relative text-[#D9D6D6] text-[11px] lg:text-[20px] md:text-[17px] sm:text-[14px] font-helvetica col-span-1"
           style={{
             borderRadius: "19px",
             boxShadow: "-2px -2px 3px 0px #FF7A00",
@@ -56,14 +56,14 @@ const JoinWorkshop = () => {
             backdropFilter: "blur(2.7000px)",
           }}
         >
-          <span className="w-[8px] h-[8px] rounded-md bg-[#fff] absolute top-4 left-4"></span>
+          <span className="w-[8px] h-[8px] sm:w-[10px] sm:h-[10px] rounded-md bg-[#fff] absolute top-4 left-4  sm:top-6 sm:left-6"></span>
 
           <img
             src="/workshop__icon__2.svg"
             alt="logo"
-            className="mx-auto mb-1"
+            className="mx-auto mb-1 lg:mb-6 sm:mb-4 lg:w-[80px] md:w-[60px] sm-w-[40px]"
           />
-          <p>Step By Step</p>
+          <p className="-mb-1">Step By Step</p>
           <p>
             Practical System
             <span className="font-bold ml-1 italic">Building</span>
@@ -71,7 +71,7 @@ const JoinWorkshop = () => {
           <img src="/Line 1.svg" alt="logo" className="mx-auto mt-2" />
         </div>
         <div
-          className="py-4 px-3 relative text-[#D9D6D6] text-[12px] font-helvetica col-span-1"
+          className="py-4 sm:py-7 lg:py-10 px-3 relative text-[#D9D6D6] text-[11px] lg:text-[20px] md:text-[17px] sm:text-[14px] font-helvetica col-span-1"
           style={{
             borderRadius: "19px",
             boxShadow: "-2px -2px 3px 0px #FF7A00",
@@ -81,14 +81,14 @@ const JoinWorkshop = () => {
             backdropFilter: "blur(2.7000px)",
           }}
         >
-          <span className="w-[8px] h-[8px] rounded-md bg-[#fff] absolute top-4 left-4"></span>
+          <span className="w-[8px] h-[8px] sm:w-[10px] sm:h-[10px] rounded-md bg-[#fff] absolute top-4 left-4  sm:top-6 sm:left-6"></span>
 
           <img
             src="/workshop__icon__3.svg"
             alt="logo"
-            className="mx-auto mb-1"
+            className="mx-auto mb-1 lg:mb-6 sm:mb-4 lg:w-[80px] md:w-[60px] sm-w-[40px]"
           />
-          <p>Proven Cold</p>
+          <p className="-mb-1">Proven Cold</p>
           <p>
             Outreach
             <span className="font-bold ml-1 italic">Tactics</span>
@@ -96,7 +96,7 @@ const JoinWorkshop = () => {
           <img src="/Line 1.svg" alt="logo" className="mx-auto mt-2" />
         </div>
         <div
-          className="py-4 px-3 relative text-[#D9D6D6] text-[12px] font-helvetica col-span-1 "
+          className="py-4 sm:py-7 lg:py-10 px-3 relative text-[#D9D6D6] text-[11px] lg:text-[20px] md:text-[17px] sm:text-[14px] font-helvetica col-span-1"
           style={{
             borderRadius: "19px",
             boxShadow: "-2px -2px 3px 0px #FF7A00",
@@ -106,14 +106,13 @@ const JoinWorkshop = () => {
             backdropFilter: "blur(2.7000px)",
           }}
         >
-          <span className="w-[8px] h-[8px] rounded-md bg-[#fff] absolute top-4 left-4"></span>
-
+          <span className="w-[8px] h-[8px] sm:w-[10px] sm:h-[10px] rounded-md bg-[#fff] absolute top-4 left-4  sm:top-6 sm:left-6"></span>
           <img
             src="/workshop__icon__4.svg"
             alt="logo"
-            className="mx-auto mb-1"
+            className="mx-auto mb-1 lg:mb-6 sm:mb-4 lg:w-[80px] md:w-[60px] sm-w-[40px]"
           />
-          <p>Client Acquisition </p>
+          <p className="-mb-1">Client Acquisition </p>
           <p>
             <span className="font-bold ml-1 italic">Mastery</span>
           </p>
